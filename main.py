db01121889f668ace1b39c5a2ef2ca53821487ea
Linus Reynolds <linusreynolds@outlook.com>
2024-08-08 09:24:54 -0700

    blub

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ bot = interactions.Client(token=TOKEN,
 
 guild_ids = [647540844873646080]
 
-@interactions.slashcommand(name="ping", description="Ping command", scope=guild_ids)
-async def ping(ctx: interactions.CommandContext):
+@interactions.slash_command(name="ping", description="Ping command", scope=guild_ids)
+async def ping(ctx: interactions.SlashContext):
     await ctx.send("Pong!")
 
-@interactions.slashcommand(name='lowestbin', description='Lowest bin command', scope=guild_ids, options=[
+@interactions.slash_command(name='lowestbin', description='Lowest bin command', scope=guild_ids, options=[
     interactions.Option(name='vertical', type=interactions.OptionType.INTEGER, description='Vertical length of move',
                         required=True), interactions.Option(name='horizontal', type=interactions.OptionType.INTEGER, description='Horizontal length of move',
                         required=True), interactions.Option(name='counts', type=interactions.OptionType.INTEGER, description='counts of move',
                         required=True)])
-async def lowestbin(ctx: interactions.CommandContext, vertical, horizontal, counts):
+async def lowestbin(ctx: interactions.SlashContext, vertical, horizontal, counts):
 
     num = pow(vertical,2) + pow(horizontal, 2)
     num = math.sqrt(num)
