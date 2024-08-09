703f4ad4e645db2bb91a093d71ebfde711d6f19b
Linus Reynolds <linusreynolds@outlook.com>
2024-08-09 15:55:47 -0700

    Made command look nicer

--- main.py
+++ main.py
@@ -15,27 +15,33 @@ bot = interactions.Client(token=TOKEN,
 
 guild_ids = [647540844873646080]
 
+
 @interactions.slash_command(name="ping", description="Ping command")
 async def ping(ctx: interactions.SlashContext):
     await ctx.send("Pong!")
 
 
-@interactions.slash_command(name='stepsize', description='blub', scopes=guild_ids, options=[
-    interactions.SlashCommandOption(name='vertical', type=interactions.OptionType.INTEGER, description='Vertical length of move',
-                        required=True), interactions.SlashCommandOption(name='horizontal', type=interactions.OptionType.INTEGER, description='Horizontal length of move',
-                        required=True), interactions.SlashCommandOption(name='counts', type=interactions.OptionType.INTEGER, description='counts of move',
-                        required=True)])
-async def lowestbin(ctx: interactions.SlashContext, vertical, horizontal, counts):
-
-    num = pow(vertical,2) + pow(horizontal, 2)
+@interactions.slash_command(name='stridesize', description='Calculates the stride size of a singular move',
+                            scopes=guild_ids, options=[
+        interactions.SlashCommandOption(name='vertical', type=interactions.OptionType.INTEGER, description='Vertical '
+                                                                                                           'length of move',
+                                        required=True),
+        interactions.SlashCommandOption(name='horizontal', type=interactions.OptionType.INTEGER,
+                                        description='Horizontal length of move',
+                                        required=True),
+        interactions.SlashCommandOption(name='counts', type=interactions.OptionType.INTEGER,
+                                        description='Count number of move',
+                                        required=True)])
+async def stridesize(ctx: interactions.SlashContext, vertical, horizontal, counts):
+    await ctx.defer()
+    num = pow(vertical, 2) + pow(horizontal, 2)
     num = math.sqrt(num)
     num /= counts
 
     num = 1 / num
     num *= 8
 
+    await ctx.send('This move is ' + str(num) + ' to 5')
 
-
-    await ctx.send(str(num))
 
 bot.start()
